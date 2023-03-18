738a47397eba58ed17d01df278a2cd87b6f08f6e
Matt Baron <mwb@unitf.net>
2023-03-18 18:17:32 -0500

    .

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mattbaron/procmon/influx"
 )
 
+const Version = "0.1.0"
+
 func main() {
+	versionFlag := flag.Bool("version", false, "Display version and exit")
+	if *versionFlag {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
+
 	processList, err := NewProcessList()
 	if err != nil {
 		os.Exit(2)
 	}
 
 	for _, processName := range os.Args[1:] {
+		processName := strings.TrimSpace(processName)
+		if len(processName) == 0 {
+			continue
+		}
+
 		line := influx.NewLine("procmon")
 		line.AddTag("name", processName)
 		line.AddField("count", processList.GetProcessCount(processName))
+
 		fmt.Println(line)
 	}
 }
